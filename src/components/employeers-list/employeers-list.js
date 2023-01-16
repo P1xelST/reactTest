b126d05dcfeee4b7b00e23fdbecdb128cfd84d26
Sergey Trifonov <se.trif0nov@yandex.com>
2023-01-17 00:31:50 +0300

    create components and use bootsrap

---
+++ src/components/employeers-list/employeers-list.js
@@ -0,0 +1,13 @@
+import EmployeersListItem from '../employeers-list-item/employeers-list-item';
+
+import './employeers-list.css';
+const EmployeersList = () => {
+    return(
+        <ul className="app-list list-group">
+            <EmployeersListItem/>
+            <EmployeersListItem/>
+            <EmployeersListItem/>
+        </ul>
+    )
+}
+export default EmployeersList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app/app';
+// const elem = <h1>hello world</h1>;
+// const text = 'Hello world';
+// const elem = (
+//   <div>
+//     <h2>Text: {text}</h2>
+//     <input type="text" />
+//     <button>Click</button>
+//   </div>
+// )
+
+/*
+работа под капотом 
+const elem = React.createElement('h2', {className: 'green'}, 'Hellow world');
+
+const element = {
+  type: 'h2',
+  props: {
+    className: 'green',
+    children: 'Hello world',
+  }
+} */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // elem,
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
